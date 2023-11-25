aca8eedfe292c8cc2241b1c72d5c87bea5534b7e
wockd3325 <wockd3325@gmail.com>
2023-11-26 03:13:47 +0900

    spring 세팅 후 커밋

---
+++ src/test/java/kr/co/kjc/spring/SpringStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.kjc.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
