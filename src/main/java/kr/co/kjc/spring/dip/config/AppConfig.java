dd9b1c287f209b9ed131fd76e46928e7ee625d3f
wockd3325 <wockd3325@gmail.com>
2023-11-29 02:54:37 +0900

    - SOLID 5원칙 - DI - IOC 컨테이너 및 DI 컨테이너 - 스프링 컨테이너 추가

--- src/main/java/kr/co/kjc/spring/dip/config/AppConfig.java
+++ src/main/java/kr/co/kjc/spring/dip/config/AppConfig.java
@@ -2,27 +2,37 @@ package kr.co.kjc.spring.dip.config;
 
 import kr.co.kjc.spring.dip.discount.DiscountPolicy;
 import kr.co.kjc.spring.dip.discount.FixDiscountPolicy;
+import kr.co.kjc.spring.dip.discount.RateDiscountPolicy;
 import kr.co.kjc.spring.dip.member.MemberService;
 import kr.co.kjc.spring.dip.member.MemberServiceImpl;
 import kr.co.kjc.spring.dip.member.MemoryMemberRepository;
 import kr.co.kjc.spring.dip.order.OrderService;
 import kr.co.kjc.spring.dip.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
+
+    @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
     }
 
-    private MemoryMemberRepository memberRepository() {
+    @Bean
+    public MemoryMemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
+    @Bean
     public DiscountPolicy discountPolicy() {
-        return new FixDiscountPolicy();
+//        return new FixDiscountPolicy();
+        return new RateDiscountPolicy();
     }
 
 }
