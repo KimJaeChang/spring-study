8899ccbdc9807d990a808aff3bac8d9c8f1f8b5a
wockd3325 <wockd3325@gmail.com>
2023-12-05 00:27:43 +0900

    애노테이션 재정의

--- src/main/java/kr/co/kjc/spring/dip/discount/RateDiscountPolicy.java
+++ src/main/java/kr/co/kjc/spring/dip/discount/RateDiscountPolicy.java
@@ -1,12 +1,14 @@
 package kr.co.kjc.spring.dip.discount;
 
+import kr.co.kjc.spring.dip.annotation.MainDiscountPolicy;
 import kr.co.kjc.spring.dip.member.Grade;
 import kr.co.kjc.spring.dip.member.Member;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
-@Qualifier("rateDiscountPolicy")
+//@Qualifier("mainDiscountPolicy")
+@MainDiscountPolicy // @Qualifier 대신 보기좋게 사용
 public class RateDiscountPolicy implements DiscountPolicy {
 
     private int discountFixAMount = 1000; // 1000원 할이
--- src/main/java/kr/co/kjc/spring/dip/order/OrderServiceImpl.java
+++ src/main/java/kr/co/kjc/spring/dip/order/OrderServiceImpl.java
@@ -1,5 +1,6 @@
 package kr.co.kjc.spring.dip.order;
 
+import kr.co.kjc.spring.dip.annotation.MainDiscountPolicy;
 import kr.co.kjc.spring.dip.discount.DiscountPolicy;
 import kr.co.kjc.spring.dip.member.Member;
 import kr.co.kjc.spring.dip.member.MemberRepository;
@@ -16,7 +17,7 @@ public class OrderServiceImpl implements OrderService {
     // spring bean 생성자 주입(Dependency Injection)
     // MemoryMemberRepository 및 FixDiscountPolicy를 의존하지 않는다!
     @Autowired
-    public OrderServiceImpl(MemberRepository memberRepository, @Qualifier("fixDiscountPolicy") DiscountPolicy discountPolicy) {
+    public OrderServiceImpl(MemberRepository memberRepository, @MainDiscountPolicy DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
     }
